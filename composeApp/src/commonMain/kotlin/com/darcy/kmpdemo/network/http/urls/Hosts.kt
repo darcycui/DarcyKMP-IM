9d95afe1851606665e1540a80870be5c79c46c6a
darcymac <darcymac@tom.com>
2026-05-29 23:55:57 +0800

    feat: change ip

--- composeApp/src/commonMain/kotlin/com/darcy/kmpdemo/network/http/urls/Hosts.kt
+++ composeApp/src/commonMain/kotlin/com/darcy/kmpdemo/network/http/urls/Hosts.kt
@@ -3,7 +3,9 @@ package com.darcy.kmpdemo.network.http.urls
 object Hosts {
     const val DOMAIN_IP = "10.0.0.241:7443"
     const val DOMAIN_IP_2 = "192.168.0.106:7443"
+    const val DOMAIN_IP_3 = "192.168.0.103:7443"
     const val DOMAIN_HOST = "www.darcycui.com.cn"
-    const val DOMAIN = DOMAIN_IP
+//    const val DOMAIN = DOMAIN_IP
 //    const val DOMAIN = DOMAIN_IP_2
+    const val DOMAIN = DOMAIN_IP_3
 }
