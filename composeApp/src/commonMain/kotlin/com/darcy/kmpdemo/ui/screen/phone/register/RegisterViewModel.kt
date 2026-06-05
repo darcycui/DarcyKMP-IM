e3b505fe81132e04cac0305f8fc89f684617fa84
darcymac <darcymac@tom.com>
2026-06-06 01:33:10 +0800

    feat: sync server-client dh after register

--- composeApp/src/commonMain/kotlin/com/darcy/kmpdemo/ui/screen/phone/register/RegisterViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/darcy/kmpdemo/ui/screen/phone/register/RegisterViewModel.kt
@@ -6,18 +6,27 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import com.darcy.kmpdemo.bean.http.error.ErrorResponse
 import com.darcy.kmpdemo.bean.http.error.toTipsIntent
 import com.darcy.kmpdemo.bean.http.request.toDTO
+import com.darcy.kmpdemo.crypto.repository.DHExchangeRepository
+import com.darcy.kmpdemo.log.logD
 import com.darcy.kmpdemo.log.logE
+import com.darcy.kmpdemo.log.logI
 import com.darcy.kmpdemo.storage.database.tables.OneTimePreKeyEntity
 import com.darcy.kmpdemo.storage.memory.IMGlobalStorage
+import com.darcy.kmpdemo.storage.memory.TransportGlobalStorage
 import com.darcy.kmpdemo.ui.base.BaseViewModel
 import com.darcy.kmpdemo.ui.base.IIntent
 import com.darcy.kmpdemo.ui.base.IReducer
+import com.darcy.kmpdemo.ui.screen.phone.login.event.LoginEvent
 import com.darcy.kmpdemo.ui.screen.phone.register.event.RegisterEvent
 import com.darcy.kmpdemo.ui.screen.phone.register.intent.RegisterIntent
 import com.darcy.kmpdemo.ui.screen.phone.register.reducer.RegisterReducer
 import com.darcy.kmpdemo.ui.screen.phone.register.repository.RegisterRepository
 import com.darcy.kmpdemo.ui.screen.phone.register.state.RegisterState
 import com.darcy.kmpdemo.utils.JsonHelper
+import com.darcy.kmpdemo.utils.hexStrToBytes
+import com.darcy.kmpdemo.utils.toBytes
+import com.darcy.kmpdemo.utils.toPublicKey
+import com.darcy.kmpdemo.x3dh.exchange.ECCExchangeHelper
 import com.darcy.kmpdemo.x3dh.usecase.GenerateIdentityKeyUseCase
 import com.darcy.kmpdemo.x3dh.usecase.GenerateOneTimePreKeysUseCase
 import com.darcy.kmpdemo.x3dh.usecase.GenerateSignedPreKeyUseCase
@@ -29,6 +38,7 @@ class RegisterViewModel(
     private val identityKeyUseCase: GenerateIdentityKeyUseCase = GenerateIdentityKeyUseCase(),
     private val signedPreKeyUseCase: GenerateSignedPreKeyUseCase = GenerateSignedPreKeyUseCase(),
     private val oneTimePreKeyUseCase: GenerateOneTimePreKeysUseCase = GenerateOneTimePreKeysUseCase(),
+    private val dhExchangeRepository: DHExchangeRepository = DHExchangeRepository(),
     private val x3DHRepository: X3DHRepository = X3DHRepository(),
 ) : BaseViewModel<RegisterState>() {
     companion object {
@@ -65,6 +75,34 @@ class RegisterViewModel(
                 onSuccess = {
                     io {
                         IMGlobalStorage.setCurrentUser(it)
+                        // 获取 Server DH公钥
+                        actionExchangeDHPublicKey(it.id)
+                    }
+                },
+                onError = {
+                    logE("注册失败：$it")
+                    main { dispatch(it.toTipsIntent()) }
+                }
+            )
+        }
+    }
+
+    private fun actionExchangeDHPublicKey(userId: Long) {
+        io {
+            val ephemeralKey = ECCExchangeHelper.generateKeyPair()
+            logD("生成临时私钥：${ephemeralKey.privateKey.toBytes().toHexString()}")
+            logD("生成临时公钥：${ephemeralKey.publicKey.toBytes().toHexString()}")
+            dhExchangeRepository.getServerDHPublicKey(
+                userId = userId,
+                publicKey = ephemeralKey.publicKey.toBytes().toHexString(),
+                onSuccess = {
+                    logI("获取 Server DH公钥成功：$it")
+                    val sharedSecret = ECCExchangeHelper.getSharedSecret(
+                        ephemeralKey.privateKey, it.publicKey.hexStrToBytes().toPublicKey()
+                    ).toHexString()
+                    io {
+                        // 保存到内存存储
+                        TransportGlobalStorage.setServerSharedSecretKey(sharedSecret)
                         val (identityKey, signedPreKey, oneTimePreKeys) = generateX3DHKeys()
                         if (identityKey.isNotEmpty() && signedPreKey.isNotEmpty() && oneTimePreKeys.isNotEmpty()) {
                             pushX3DHKeys(identityKey, signedPreKey, oneTimePreKeys)
@@ -75,10 +113,9 @@ class RegisterViewModel(
                     }
                 },
                 onError = {
-                    logE("注册失败：$it")
+                    logE("获取 Server DH公钥失败：$it")
                     main { dispatch(it.toTipsIntent()) }
-                }
-            )
+                })
         }
     }
 
