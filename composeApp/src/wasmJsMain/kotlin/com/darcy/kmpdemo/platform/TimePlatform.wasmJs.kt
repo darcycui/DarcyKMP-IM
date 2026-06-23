354fd30d538049fc723e939349598532d950ef2b
Tom_docker <Tom_docker@jerry.com>
2026-06-23 17:28:45 +0800

    fix: Kotlin/Wasm call  js() error

--- composeApp/src/wasmJsMain/kotlin/com/darcy/kmpdemo/platform/TimePlatform.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/com/darcy/kmpdemo/platform/TimePlatform.wasmJs.kt
@@ -1,7 +1,12 @@
 package com.darcy.kmpdemo.platform
 
+@OptIn(ExperimentalWasmJsInterop::class)
+private fun getCurrentTimeStampImpl(): String =
+    js("new Date().toISOString().replace('T', ' ').slice(0, -1)")
+
+
 actual object TimePlatform {
     actual fun getCurrentTimeStamp(): String {
-        return js("new Date().toISOString().replace('T', ' ').slice(0, -1)") as String
+        return getCurrentTimeStampImpl()
     }
 }
