a262147c764d5c95ef4a9e3bddef623f6fd45586
Tom_docker <Tom_docker@jerry.com>
2026-06-24 16:59:26 +0800

    feat: use 7443 port

--- composeApp/src/commonMain/kotlin/com/darcy/kmpdemo/network/http/urls/Hosts.kt
+++ composeApp/src/commonMain/kotlin/com/darcy/kmpdemo/network/http/urls/Hosts.kt
@@ -1,7 +1,7 @@
 package com.darcy.kmpdemo.network.http.urls
 
 object Hosts {
-    const val PORT = "7777"
+    const val PORT = "7443"
     const val DOMAIN_IP = "10.0.0.241:$PORT"
     const val DOMAIN_IP_2 = "192.168.0.101:$PORT"
     const val DOMAIN_HOST = "www.darcycui.com.cn"
