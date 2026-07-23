64ef7dffad90fdcff37a5f3c002598979da3142d
darcymac <darcymac@tom.com>
2026-07-23 16:00:38 +0800

    feat: run on darcy server ip

--- composeApp/src/commonMain/kotlin/com/darcy/kmpdemo/network/http/urls/Hosts.kt
+++ composeApp/src/commonMain/kotlin/com/darcy/kmpdemo/network/http/urls/Hosts.kt
@@ -1,10 +1,9 @@
 package com.darcy.kmpdemo.network.http.urls
 
 object Hosts {
-    const val PORT = "7443"
-    const val DOMAIN_IP = "10.0.0.241:$PORT"
-    const val DOMAIN_IP_2 = "192.168.0.101:$PORT"
+    const val DOMAIN_IP = "123.57.106.106:7443"
+    const val DOMAIN_IP_2 = "192.168.0.104:7443"
     const val DOMAIN_HOST = "www.darcycui.com.cn"
-    const val DOMAIN = DOMAIN_IP
-//    const val DOMAIN = DOMAIN_IP_2
+//    const val DOMAIN = DOMAIN_IP
+    const val DOMAIN = DOMAIN_IP_2
 }
